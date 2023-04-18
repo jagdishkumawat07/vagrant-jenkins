58dfe855ba41d00849e162c236a6f4bb6f321418
jagdishkumawat07 <jagdishkumawat07@gmail.com>
2023-04-18 16:27:38 +0530

    updated

---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+ubuntu="/home/main/Desktop/vagrant/ubuntu"
+redhat="/home/main/Desktop/vagrant/redhat"
+
+repoRoot=$(realpath "$(dirname "$0")")
+
+cd "$repoRoot"
+
+## sh "./run.sh '${component}'" 
+
+
+COMPONENT="$1"
+if [[ -z "$COMPONENT" ]]; then
+    echo "COMPONENT is not set"
+    exit 1
+fi
+
+case "$COMPONENT" in
+ubuntu)
+    mkdir -p $ubuntu
+    cd $ubuntu
+    curl -H "Authorization: token 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" -o Vagrantfile https://raw.githubusercontent.com/shivangiverma369/vagrant-private/main/ubuntu/Vagrantfile
+    vagrant up
+    ;;
+ubuntu_clean-up)
+    cd $ubuntu
+    vagrant halt && vagrant destroy -f
+    ;;
+redhat)
+    mkdir -p $redhat
+    cd $redhat
+    curl -H "Authorization: token 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" -o Vagrantfile https://raw.githubusercontent.com/shivangiverma369/vagrant-private/main/redhat/Vagrantfile
+    vagrant up
+    ;;
+redhat_clean-up)
+    cd $redhat
+    vagrant halt && vagrant destroy -f
+    ;;
+reset-ubuntu)
+    cd $ubuntu
+    vagrant halt && vagrant destroy -f 
+    vagrant up
+   ;;
+reset-redhat)
+    cd $redhat
+    vagrant halt && vagrant destroy -f 
+    vagrant up
+   ;;         
+*) ;;
+esac
